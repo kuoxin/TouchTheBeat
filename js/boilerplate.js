62168e82e76ecd339139d213e2b61655b0909231
Hermann <hermannstolte@gmail.com>
2014-07-13 19:29:24 +0200

    first minified version

--- js/boilerplate.js
+++
@@ -1,8 +0,0 @@
-
-define([
-  'jquery',     // lib/jquery/jquery
-  'underscore', // lib/underscore/underscore
-  'backbone'    // lib/backbone/backbone
-], function($, _, Backbone){
-  return {};
-});
