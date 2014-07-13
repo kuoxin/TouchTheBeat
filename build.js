a5449fdd2f44c65fd15bb74186442779f383aa4d
Hermann <hermannstolte@gmail.com>
2014-07-13 22:27:04 +0200

    improvements for r.js build, added r.js and build configuration to repo

---
+++ build.js
@@ -0,0 +1,14 @@
+({
+    baseUrl: "js",
+    appDir: "./",
+    modules: [
+        {
+            name: "main"
+        }
+    ],
+
+    preserveLicenseComments: false,
+    mainConfigFile: 'js/main.js',
+    inlineText: true,
+    dir: "../ttb-build"
+})
