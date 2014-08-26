66609b1c9900838fd80e24d1a54998cdfe0593c9
Hermann <hermannstolte@gmail.com>
2014-08-26 18:29:47 +0200

    edited the level-overview, removed old remains from the level-generator instructions, closes #31

--- js/views/chooselevel.js
+++ js/views/chooselevel.js
@@ -10,42 +10,81 @@ define([
     'app'
 ], function ($, _, Backbone, levelvalidator, LevelOverviewTemplate, LevelPanel, PlayView, levelcontainer, app) {
     var ChooseLevelView = Backbone.View.extend({
-
         render: function () {
             Backbone.history.navigate('chooselevel', {trigger: false, replace: true});
             var template = _.template(LevelOverviewTemplate, {});
             this.$el.html(template);
             this.levelpanelviews = [];
-            var containerelem = $('#levelcontainer');
-            containerelem.html('');
+            this.containerelement_packaged = $('#levelcontainer_packaged');
+            this.containerelement_custom = $('#levelcontainer_custom');
+            this.inputstatediv = $('#inputstatediv');
+            this.containerelement_packaged.html('');
             for (var i = 0; i < levelcontainer.length; i++) {
                 var clevel = levelcontainer[i];
                 var view = new LevelPanel();
                 view.render(clevel);
-                containerelem.append(view.el);
+                this.containerelement_packaged.append(view.el);
                 this.levelpanelviews.push(view);
             }
         },
+
         events: {
-            'click #startlevel': 'startlevel',
-            'click #demolevel': 'startdemo',
-            'click #devbutton': 'devaction',
-            'click #easy': 'starteasy',
-            'click #intermediate': 'startintermediate',
-            'click #hard': 'starthard'
+            'input #leveljsoninput': 'inputchanged'
         },
 
-        startlevel: function () {
-            if (levelvalidator.validate($('#leveljsoninput').val())) {
-                app.startlevel(JSON.parse($('#leveljsoninput').val()));
+        inputchanged: function () {
+            var value = $('#leveljsoninput').val();
+
+            if (value == '') {
+                //value is empty
+                this.inputstatediv.removeClass('has-success has-error');
+                this.containerelement_custom.slideUp();
+
+                if (this.$("#alert_invalidlevel").is(":visible")) {
+                    this.$("#alert_invalidlevel").slideUp();
+                }
+
+                if (this.containerelement_custom.is(":visible")) {
+                    this.containerelement_custom.slideUp();
+                }
+
+
             }
             else {
-                this.invalidLevelJSON();
-            }
-        },
+                //value is not empty
+                if (levelvalidator.validate(value)) {
+                    // input is valid
 
-        invalidLevelJSON: function () {
-            this.$("#alert_invalidlevel").slideDown();
+                    this.inputstatediv.removeClass('has-error').addClass('has-success');
+                    if (this.$("#alert_invalidlevel").is(":visible")) {
+                        this.$("#alert_invalidlevel").slideUp();
+                    }
+
+                    var view = new LevelPanel();
+                    view.render(JSON.parse(value));
+                    this.containerelement_custom.html(view.el);
+
+                    if (!this.containerelement_custom.is(":visible")) {
+                        this.containerelement_custom.slideDown();
+                    }
+                }
+                else {
+                    //input is invalid
+                    this.inputstatediv.removeClass('has-success').addClass('has-error');
+                    if (this.containerelement_custom.is(":visible")) {
+                        this.containerelement_custom.slideUp();
+                    }
+
+                    this.$("#alert_invalidlevel").slideDown();
+
+                    if (this.containerelement_custom.is(":visible")) {
+                        this.containerelement_custom.slideUp();
+                    }
+
+                }
+            }
+
+
         }
     });
     return ChooseLevelView;
