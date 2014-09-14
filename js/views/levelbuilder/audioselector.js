abff731a98fc3b8daa7e5a34432a43ff076eba0d
Hermann <hermannstolte@gmail.com>
2014-09-14 17:53:28 +0200

    removed dead link in track recommendations

--- js/views/levelbuilder/audioselector.js
+++ js/views/levelbuilder/audioselector.js
@@ -20,7 +20,7 @@ define([
                 'https://soundcloud.com/greco-roman/roosevelt-sea-1',
                 //'https://soundcloud.com/t-e-e-d/garden',
                 'https://soundcloud.com/homoheadphonico/phantogram-don-t-move',
-                'https://soundcloud.com/etagenoir/parov-stelar-catgroove',
+                //'https://soundcloud.com/etagenoir/parov-stelar-catgroove', it does no longer exist
                 'https://soundcloud.com/fosterthepeoplemusic/pumpedupkicks',
                 'https://soundcloud.com/benkhan/savage',
                 //'https://soundcloud.com/kooksmusic/forgive-forget'
