271c31b755ad6b034b585d603d3096e313b160c5
Hermann <hermannstolte@gmail.com>
2014-11-27 16:50:25 +0100

    further preparations for continuous deployment (6)

--- Gruntfile.js
+++ Gruntfile.js
@@ -122,7 +122,7 @@ module.exports = function (grunt) {
         grunt.task.requires('checkTravisTrustedEnvironment');
 
         //for temporal debugging purposes
-        grunt.log.writeln(JSON.stringify(process.env));
+        grunt.log.writeln(process.env.test);
 
         if (typeof process.env[VARNAME_ENCRYPTIONKEY] === 'undefined')
             grunt.fail.warn("Travis environment variable \"encryption_key\" missing. Stopping deploy.");
